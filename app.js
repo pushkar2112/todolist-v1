1eed25856020d1a9eed5e43b14671dd3a0cf364c
Pushkar Verma <pushkar7397@gmail.com>
2022-06-11 22:46:47 +0530

    Add EJS support

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
   var today = new Date();
   var currentDay = today.getDay()
