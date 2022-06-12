c1d6e00d2e5e977d8c21df87e68010d930e83147
Pushkar Verma <pushkar7397@gmail.com>
2022-06-12 18:27:46 +0530

    Add work page and configure it

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs'); //add ejs support for templating
 
@@ -23,17 +24,29 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {kindOfDay : day, newListItems : items}) //Add a variable to change as per the needs
+  res.render('list', {listTitle : day, newListItems : items}) //Add a variable to change as per the needs
 
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    
-    items.push(item);
 
-    res.redirect("/");
+    if (req.body.newItem === "work"){
+      workItems.push(item)
+      res.redirect("/work")
+    } else {
+      items.push(item);
+      res.redirect("/");
+      
+    }
+    
+    
 });
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
