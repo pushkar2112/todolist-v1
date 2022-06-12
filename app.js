dd497de205571bcf0c5706882c84b843b78fae2a
Pushkar Verma <pushkar7397@gmail.com>
2022-06-12 13:25:55 +0530

    manage list items in order using EJS scriplet tags

--- app.js
+++ app.js
@@ -4,44 +4,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = [];
 
 app.set('view engine', 'ejs'); //add ejs support for templating
 
 app.get("/", function(req, res){
   var today = new Date();
-  var currentDay = today.getDay()
-  var day = ""
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday"
-      break;
-    case 1:
-      day = "Monday"
-      break;
-    case 2:
-      day = "Tuesday"
-      break;
-    case 3:
-      day = "Wednesday"
-      break;
-    case 4:
-      day = "Thursday"
-      break;
-    case 5:
-      day = "Friday"
-      break;
-    case 6:
-      day = "Saturday"
-      break;
-  
-    default:
-      console.log("ERROR: Current Day is equal to: " + currentDay);
-      break;
+  var options = {
+    weekday : "long",
+    day : "numeric",
+    month : "long"
   }
 
-  res.render('list', {kindOfDay : day}) //Add a variable to change as per the needs
+  var day = today.toLocaleDateString("en-US", options);
 
+  res.render('list', {kindOfDay : day, newListItems : items}) //Add a variable to change as per the needs
+
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    
+    items.push(item);
+
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
